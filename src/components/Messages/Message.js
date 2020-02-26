6be547ec410564337ae5d4c1114688a469e9666e
Thuyan Duong <thuyan.duong@flatironschool.com>
2020-02-25 21:18:38 -0500

    fixed messages

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -5,7 +5,7 @@ import {removeMessage} from '../../redux/actionCreators'
 const Message = props => {
   let {content, id, type} = props.message
   return (
-    <div className={`ui floating message ${type}`}>
+    <div className={`ui floating message fade ${type}`}>
       <i className="close icon" onClick={()=>{props.removeMessage(id)}}></i>
       {content}
     </div>
